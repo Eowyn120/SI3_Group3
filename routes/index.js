a4d2acbbdfb4d60599a539bc96e962a35e38c7e0
Eowyn120 <147740904+Eowyn120@users.noreply.github.com>
2025-05-11 03:03:47 -0400

    views

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,35 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Proyecto Nutricion' });
+  res.render('index', { title: 'Nutricode' });
 });
+
+router.get('/nosotros', function(req, res, next){
+  res.render('about', { title: 'Nosotros'});
+});
+
+router.get('/clientes', function(req, res, next){
+  res.render('clients', {title: 'Clientes'});
+});
+
+router.get('/mision', function(req, res, next){
+  res.render('ourwork', {title: 'Mision'});
+});
+
+router.get('/register', function(req, res, next){
+  res.render('register', {title: 'Registro'});
+});
+
+router.get('/login', function(req, res, next){
+  res.render('login', {title: 'Inicio de Sesión'});
+});
+
+router.post('/register', function(req, res, next){
+  const {nombre, apellido, email, password, preg, seg} = req.body;
+  console.log(req.body);
+  res.redirect('/login');
+  console.log('Registro funciona');
+  
+})
 
 module.exports = router;
